4bdceed950202c48392079002925725b01231f3a
RobRorr <84281916+RobRorr@users.noreply.github.com>
2023-11-29 18:56:51 +0100

    Fix admin_id

--- santabot.py
+++ santabot.py
@@ -10,7 +10,7 @@ config_file_path = os.path.normpath('config.ini')
 config = setconfig(config_file_path)
 json_file_path = config.get('json_file_path')
 telegram_token = config.get('telegram_token')
-admin_id = config.get('admin')
+admin_id = int(config.get('admin'))
 
 bot = telebot.TeleBot(telegram_token)
 
