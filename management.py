33b9fc7c5e554f6c012cea315e1ae896d549a8c2
RobRorr <84281916+RobRorr@users.noreply.github.com>
2024-11-01 12:12:30 +0100

    renamed dictionary usage

--- management.py
+++ management.py
@@ -10,7 +10,7 @@ import bot_config
 from languages import dictionary
 
 bot = telebot.TeleBot(bot_config.telegram_token)
-language = bot_config.language
+lex = dictionary[bot_config.language]
 
 
 def add_participant(participant_list, element):
@@ -21,17 +21,17 @@ def create_list_step(message):
     if os.path.exists(bot_config.json_file_path):
         data = read_data(message)
         if message.text in data:
-            bot.reply_to(message, dictionary[language]['group_exists'])
+            bot.reply_to(message, lex['group_exists'])
             return
         else:
             data[message.text] = []
             write_data(message, data)
-            bot.reply_to(message, dictionary[language]['group_added'])
+            bot.reply_to(message, lex['group_added'])
             return
     else:
         data = {message.text: []}
         write_data(message, data)
-        bot.reply_to(message, dictionary[language]['group_added'])
+        bot.reply_to(message, lex['group_added'])
 
 
 def remove_all_participants(message):
@@ -39,7 +39,7 @@ def remove_all_participants(message):
     for participant_list in data:
         data[participant_list] = []
     write_data(message, data)
-    bot.reply_to(message, dictionary[language]['done'])
+    bot.reply_to(message, lex['done'])
 
 
 def remove_id_step(message):
@@ -47,7 +47,7 @@ def remove_id_step(message):
     for participant_list in data:
         data[participant_list] = remove_participant(data[participant_list], message.text)
     write_data(message, data)
-    bot.reply_to(message, dictionary[language]['done'])
+    bot.reply_to(message, lex['done'])
 
 
 def remove_participant(participant_list, element):
@@ -59,7 +59,7 @@ def secret_santa(message):
     if message.text in data:
         start_secret_santa(data[message.text])
     else:
-        bot.reply_to(message, dictionary[language]['group_no_exists'])
+        bot.reply_to(message, lex['group_no_exists'])
 
 
 def secret_santa_all(message):
@@ -73,9 +73,9 @@ def start_secret_santa(participant_list):
     for i in range(len(participant_list)):
         recipient = participant_list[(i + 1) % len(participant_list)]
         if not validators.url(recipient[2]):
-            msg = dictionary[language]['recipient'] + recipient[1]
+            msg = lex['recipient'] + recipient[1]
         else:
-            msg = dictionary[language]['recipient'] + recipient[1] + " : " + recipient[2]
+            msg = lex['recipient'] + recipient[1] + " : " + recipient[2]
         bot.send_message(participant_list[i][0], msg)
 
 
@@ -88,7 +88,7 @@ def read_data(message):
         with open(bot_config.json_file_path, 'r') as f:
             data = json.load(f)
     except Exception as e:
-        bot.reply_to(message, dictionary[language]['error'])
+        bot.reply_to(message, lex['error'])
         logging.debug(e)
     return data
 
@@ -98,5 +98,5 @@ def write_data(message, data):
         with open(bot_config.json_file_path, 'w') as f:
             json.dump(data, f)
     except Exception as e:
-        bot.reply_to(message, dictionary[language]['error'])
+        bot.reply_to(message, lex['error'])
         logging.debug(e)
